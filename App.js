af38992346308f4b44f0228544efb3f5a7223514
NguyenHuuHoa29 <hoacrkh10@gmail.com>
2023-05-23 01:59:08 +0700

    up2

--- App.js
+++ App.js
@@ -31,20 +31,20 @@ const StackBegin = () => {
 }
 const App = () => {
   return (
-    // <NavigationContainer>
-    //     <Stack.Navigator initialRouteName="BottomTabs" screenOptions={{ headerShown: false }}>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="BottomTabs" screenOptions={{ headerShown: false }}>
           
-    //       <Stack.Screen name="StackBegin" component={StackBegin} />
-    //       <Stack.Screen name="Profile" component={Profile} />
-    //       <Stack.Screen name="BottomTabs" component={BottomTabs} />
-    //       <Stack.Screen name="AddNew" component={AddNew} />
-    //       <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="StackBegin" component={StackBegin} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="BottomTabs" component={BottomTabs} />
+          <Stack.Screen name="AddNew" component={AddNew} />
+          <Stack.Screen name="Home" component={Home} />
 
-    //       <Stack.Screen name="Test" component={Test} />
-    //       <Stack.Screen name="ItemDishes" component={ItemDishes} />
-    //     </Stack.Navigator>
-    //   </NavigationContainer>
-    <AddNew></AddNew>
+          <Stack.Screen name="Test" component={Test} />
+          <Stack.Screen name="ItemDishes" component={ItemDishes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+ 
   )
 }
 
