3754aa8d7da3be6eab68f49da2d4a949137b358e
SonHyBuild <sonhy280@gmail.com>
2023-06-01 16:49:04 +0700

    countDownSearch

--- screens/MainTabs/Search.js
+++ screens/MainTabs/Search.js
@@ -1,20 +1,52 @@
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity,Dimensions, Image } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Dimensions, Image } from 'react-native'
+import React, { useState } from 'react'
 import { TextInput } from 'react-native-paper'
 import { COLOR, ICON } from '../../constants/Themes'
 const windowWIdth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+import AxiosIntance from '../../constants/AxiosIntance';
 const Search = () => {
+    let timeOut = null;
+    const countdownSearch = (searchText) => {
+        if (timeOut) {
+            clearTimeout(timeOut);
+        }
+        timeOut = setTimeout(() => {
+            search(searchText);
+        }, 3000);
+    }
+    const search = async (searchText) => {
+        //http://localhost:3001
+        //setisLoading(true);
+        try {
+            console.log(searchText);
+            const response = await AxiosIntance().post("/recipe/api/search-by-title", { title: searchText });
+            console.log(response);
+            // if (response.error === false) {
+            //     setdataNe(response.products);
+            //     setisLoading(false);
+            // }
+            // else {
+            //     ToastAndroid.show("Thất bại", ToastAndroid.SHORT);
+            // }
+        } catch (error) {
+
+        }
+
+
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity >
+                <TouchableOpacity onPress={search} >
                     <Image source={ICON.Search} style={styles.imageSearch}></Image>
                 </TouchableOpacity>
-                <TextInput 
-                placeholder='Gõ vào tên món ăn ...' 
-                placeholderTextColor={COLOR.WHITE}
-                style={styles.input}></TextInput>
+                <TextInput
+                    onChangeText={(text) => countdownSearch(text)}
+                    placeholder='Gõ vào tên món ăn ...'
+                    placeholderTextColor={COLOR.WHITE}
+                    style={styles.input}></TextInput>
             </View>
         </SafeAreaView>
     )
@@ -27,12 +59,12 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: COLOR.BACKGROUND
     },
-    header: {   
+    header: {
         flexDirection: 'row',
-        height:'10%',
+        height: '10%',
         backgroundColor: COLOR.BACKGROUND3,
-        justifyContent:'flex-start',
-        alignItems:'center',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
 
     },
     input: {
